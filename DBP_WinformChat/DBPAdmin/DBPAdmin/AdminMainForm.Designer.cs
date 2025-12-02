e9c8644d6b154d1cc6a2855e8d237a409c5826f6
ayzenhq <ihayeong386@gmail.com>
2025-12-02 14:45:52 +0900

    ｕｉ수정

--- DBP_WinformChat/DBPAdmin/DBPAdmin/AdminMainForm.Designer.cs
+++ DBP_WinformChat/DBPAdmin/DBPAdmin/AdminMainForm.Designer.cs
@@ -22,160 +22,180 @@
 
         private void InitializeComponent()
         {
-            this.pnlLeft = new System.Windows.Forms.Panel();
-            this.lblLogo = new System.Windows.Forms.Label();
-            this.btnDashboard = new System.Windows.Forms.Button();
-            this.btnDepartment = new System.Windows.Forms.Button();
-            this.btnUsers = new System.Windows.Forms.Button();
-            this.btnChatSearch = new System.Windows.Forms.Button();
-            this.btnLoginLog = new System.Windows.Forms.Button();
-            this.btnPermission = new System.Windows.Forms.Button();
-            this.pnlContent = new System.Windows.Forms.Panel();
-            this.pnlLeft.SuspendLayout();
-            this.SuspendLayout();
-
-            // ========== pnlLeft (사이드바) ==========
-            this.pnlLeft.BackColor = System.Drawing.ColorTranslator.FromHtml("#778873");
-            this.pnlLeft.Controls.Add(this.lblLogo);
-            this.pnlLeft.Controls.Add(this.btnDashboard);
-            this.pnlLeft.Controls.Add(this.btnDepartment);
-            this.pnlLeft.Controls.Add(this.btnUsers);
-            this.pnlLeft.Controls.Add(this.btnChatSearch);
-            this.pnlLeft.Controls.Add(this.btnLoginLog);
-            this.pnlLeft.Controls.Add(this.btnPermission);
-            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
-            this.pnlLeft.Name = "pnlLeft";
-            this.pnlLeft.Size = new System.Drawing.Size(220, 661);
-            this.pnlLeft.TabIndex = 0;
-
-            // ========== lblLogo ==========
-            this.lblLogo.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
-            this.lblLogo.ForeColor = System.Drawing.Color.White;
-            this.lblLogo.Location = new System.Drawing.Point(0, 20);
-            this.lblLogo.Name = "lblLogo";
-            this.lblLogo.Size = new System.Drawing.Size(220, 50);
-            this.lblLogo.TabIndex = 0;
-            this.lblLogo.Text = "관리자 패널";
-            this.lblLogo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
-            // ========== btnDashboard ==========
-            this.btnDashboard.BackColor = System.Drawing.ColorTranslator.FromHtml("#A1BC98");
-            this.btnDashboard.FlatAppearance.BorderSize = 0;
-            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDashboard.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
-            this.btnDashboard.ForeColor = System.Drawing.Color.White;
-            this.btnDashboard.Location = new System.Drawing.Point(10, 90);
-            this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnDashboard.Size = new System.Drawing.Size(200, 42);
-            this.btnDashboard.TabIndex = 1;
-            this.btnDashboard.Text = "대시보드";
-            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDashboard.UseVisualStyleBackColor = false;
-            this.btnDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
-
-            // ========== btnDepartment ==========
-            this.btnDepartment.BackColor = System.Drawing.ColorTranslator.FromHtml("#778873");
-            this.btnDepartment.FlatAppearance.BorderSize = 0;
-            this.btnDepartment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDepartment.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
-            this.btnDepartment.ForeColor = System.Drawing.ColorTranslator.FromHtml("#D2DCB6");
-            this.btnDepartment.Location = new System.Drawing.Point(10, 140);
-            this.btnDepartment.Name = "btnDepartment";
-            this.btnDepartment.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnDepartment.Size = new System.Drawing.Size(200, 42);
-            this.btnDepartment.TabIndex = 2;
-            this.btnDepartment.Text = "부서관리";
-            this.btnDepartment.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDepartment.UseVisualStyleBackColor = false;
-            this.btnDepartment.Cursor = System.Windows.Forms.Cursors.Hand;
-
-            // ========== btnUsers ==========
-            this.btnUsers.BackColor = System.Drawing.ColorTranslator.FromHtml("#778873");
-            this.btnUsers.FlatAppearance.BorderSize = 0;
-            this.btnUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUsers.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
-            this.btnUsers.ForeColor = System.Drawing.ColorTranslator.FromHtml("#D2DCB6");
-            this.btnUsers.Location = new System.Drawing.Point(10, 190);
-            this.btnUsers.Name = "btnUsers";
-            this.btnUsers.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnUsers.Size = new System.Drawing.Size(200, 42);
-            this.btnUsers.TabIndex = 3;
-            this.btnUsers.Text = "사용자관리";
-            this.btnUsers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnUsers.UseVisualStyleBackColor = false;
-            this.btnUsers.Cursor = System.Windows.Forms.Cursors.Hand;
-
-            // ========== btnChatSearch ==========
-            this.btnChatSearch.BackColor = System.Drawing.ColorTranslator.FromHtml("#778873");
-            this.btnChatSearch.FlatAppearance.BorderSize = 0;
-            this.btnChatSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnChatSearch.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
-            this.btnChatSearch.ForeColor = System.Drawing.ColorTranslator.FromHtml("#D2DCB6");
-            this.btnChatSearch.Location = new System.Drawing.Point(10, 240);
-            this.btnChatSearch.Name = "btnChatSearch";
-            this.btnChatSearch.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnChatSearch.Size = new System.Drawing.Size(200, 42);
-            this.btnChatSearch.TabIndex = 4;
-            this.btnChatSearch.Text = "대화내용검색";
-            this.btnChatSearch.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnChatSearch.UseVisualStyleBackColor = false;
-            this.btnChatSearch.Cursor = System.Windows.Forms.Cursors.Hand;
-
-            // ========== btnLoginLog ==========
-            this.btnLoginLog.BackColor = System.Drawing.ColorTranslator.FromHtml("#778873");
-            this.btnLoginLog.FlatAppearance.BorderSize = 0;
-            this.btnLoginLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLoginLog.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
-            this.btnLoginLog.ForeColor = System.Drawing.ColorTranslator.FromHtml("#D2DCB6");
-            this.btnLoginLog.Location = new System.Drawing.Point(10, 290);
-            this.btnLoginLog.Name = "btnLoginLog";
-            this.btnLoginLog.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnLoginLog.Size = new System.Drawing.Size(200, 42);
-            this.btnLoginLog.TabIndex = 5;
-            this.btnLoginLog.Text = "로그인기록";
-            this.btnLoginLog.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLoginLog.UseVisualStyleBackColor = false;
-            this.btnLoginLog.Cursor = System.Windows.Forms.Cursors.Hand;
-
-            // ========== btnPermission ==========
-            this.btnPermission.BackColor = System.Drawing.ColorTranslator.FromHtml("#778873");
-            this.btnPermission.FlatAppearance.BorderSize = 0;
-            this.btnPermission.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPermission.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
-            this.btnPermission.ForeColor = System.Drawing.ColorTranslator.FromHtml("#D2DCB6");
-            this.btnPermission.Location = new System.Drawing.Point(10, 340);
-            this.btnPermission.Name = "btnPermission";
-            this.btnPermission.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.btnPermission.Size = new System.Drawing.Size(200, 42);
-            this.btnPermission.TabIndex = 6;
-            this.btnPermission.Text = "권한관리";
-            this.btnPermission.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPermission.UseVisualStyleBackColor = false;
-            this.btnPermission.Cursor = System.Windows.Forms.Cursors.Hand;
-
-            // ========== pnlContent (메인 영역) ==========
-            this.pnlContent.BackColor = System.Drawing.ColorTranslator.FromHtml("#F1F3E0");
-            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlContent.Location = new System.Drawing.Point(220, 0);
-            this.pnlContent.Name = "pnlContent";
-            this.pnlContent.Padding = new System.Windows.Forms.Padding(20);
-            this.pnlContent.Size = new System.Drawing.Size(980, 661);
-            this.pnlContent.TabIndex = 1;
-            this.pnlContent.AutoScroll = true;
-
-            // ========== AdminMainForm ==========
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 661);
-            this.Controls.Add(this.pnlContent);
-            this.Controls.Add(this.pnlLeft);
-            this.Name = "AdminMainForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "관리자 패널";
-            this.pnlLeft.ResumeLayout(false);
-            this.ResumeLayout(false);
+            pnlLeft = new Panel();
+            lblLogo = new Label();
+            btnDashboard = new Button();
+            btnDepartment = new Button();
+            btnUsers = new Button();
+            btnChatSearch = new Button();
+            btnLoginLog = new Button();
+            btnPermission = new Button();
+            pnlContent = new Panel();
+            pnlLeft.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            pnlLeft.BackColor = Color.FromArgb(119, 136, 115);
+            pnlLeft.Controls.Add(lblLogo);
+            pnlLeft.Controls.Add(btnDashboard);
+            pnlLeft.Controls.Add(btnDepartment);
+            pnlLeft.Controls.Add(btnUsers);
+            pnlLeft.Controls.Add(btnChatSearch);
+            pnlLeft.Controls.Add(btnLoginLog);
+            pnlLeft.Controls.Add(btnPermission);
+            pnlLeft.Dock = DockStyle.Left;
+            pnlLeft.Location = new Point(0, 0);
+            pnlLeft.Margin = new Padding(4, 5, 4, 5);
+            pnlLeft.Name = "pnlLeft";
+            pnlLeft.Size = new Size(283, 759);
+            pnlLeft.TabIndex = 0;
+            // 
+            // lblLogo
+            // 
+            lblLogo.Font = new Font("맑은 고딕", 14F, FontStyle.Bold);
+            lblLogo.ForeColor = Color.White;
+            lblLogo.Location = new Point(0, 33);
+            lblLogo.Margin = new Padding(4, 0, 4, 0);
+            lblLogo.Name = "lblLogo";
+            lblLogo.Size = new Size(283, 83);
+            lblLogo.TabIndex = 0;
+            lblLogo.Text = "관리자 패널";
+            lblLogo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.BackColor = Color.FromArgb(161, 188, 152);
+            btnDashboard.Cursor = Cursors.Hand;
+            btnDashboard.FlatAppearance.BorderSize = 0;
+            btnDashboard.FlatStyle = FlatStyle.Flat;
+            btnDashboard.Font = new Font("맑은 고딕", 10F, FontStyle.Bold);
+            btnDashboard.ForeColor = Color.White;
+            btnDashboard.Location = new Point(13, 150);
+            btnDashboard.Margin = new Padding(4, 5, 4, 5);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Padding = new Padding(19, 0, 0, 0);
+            btnDashboard.Size = new Size(257, 70);
+            btnDashboard.TabIndex = 1;
+            btnDashboard.Text = "대시보드";
+            btnDashboard.TextAlign = ContentAlignment.MiddleLeft;
+            btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // btnDepartment
+            // 
+            btnDepartment.BackColor = Color.FromArgb(119, 136, 115);
+            btnDepartment.Cursor = Cursors.Hand;
+            btnDepartment.FlatAppearance.BorderSize = 0;
+            btnDepartment.FlatStyle = FlatStyle.Flat;
+            btnDepartment.Font = new Font("맑은 고딕", 10F, FontStyle.Bold);
+            btnDepartment.ForeColor = Color.FromArgb(210, 220, 182);
+            btnDepartment.Location = new Point(13, 233);
+            btnDepartment.Margin = new Padding(4, 5, 4, 5);
+            btnDepartment.Name = "btnDepartment";
+            btnDepartment.Padding = new Padding(19, 0, 0, 0);
+            btnDepartment.Size = new Size(257, 70);
+            btnDepartment.TabIndex = 2;
+            btnDepartment.Text = "부서관리";
+            btnDepartment.TextAlign = ContentAlignment.MiddleLeft;
+            btnDepartment.UseVisualStyleBackColor = false;
+            // 
+            // btnUsers
+            // 
+            btnUsers.BackColor = Color.FromArgb(119, 136, 115);
+            btnUsers.Cursor = Cursors.Hand;
+            btnUsers.FlatAppearance.BorderSize = 0;
+            btnUsers.FlatStyle = FlatStyle.Flat;
+            btnUsers.Font = new Font("맑은 고딕", 10F, FontStyle.Bold);
+            btnUsers.ForeColor = Color.FromArgb(210, 220, 182);
+            btnUsers.Location = new Point(13, 317);
+            btnUsers.Margin = new Padding(4, 5, 4, 5);
+            btnUsers.Name = "btnUsers";
+            btnUsers.Padding = new Padding(19, 0, 0, 0);
+            btnUsers.Size = new Size(257, 70);
+            btnUsers.TabIndex = 3;
+            btnUsers.Text = "사용자관리";
+            btnUsers.TextAlign = ContentAlignment.MiddleLeft;
+            btnUsers.UseVisualStyleBackColor = false;
+            // 
+            // btnChatSearch
+            // 
+            btnChatSearch.BackColor = Color.FromArgb(119, 136, 115);
+            btnChatSearch.Cursor = Cursors.Hand;
+            btnChatSearch.FlatAppearance.BorderSize = 0;
+            btnChatSearch.FlatStyle = FlatStyle.Flat;
+            btnChatSearch.Font = new Font("맑은 고딕", 10F, FontStyle.Bold);
+            btnChatSearch.ForeColor = Color.FromArgb(210, 220, 182);
+            btnChatSearch.Location = new Point(13, 400);
+            btnChatSearch.Margin = new Padding(4, 5, 4, 5);
+            btnChatSearch.Name = "btnChatSearch";
+            btnChatSearch.Padding = new Padding(19, 0, 0, 0);
+            btnChatSearch.Size = new Size(257, 70);
+            btnChatSearch.TabIndex = 4;
+            btnChatSearch.Text = "대화내용검색";
+            btnChatSearch.TextAlign = ContentAlignment.MiddleLeft;
+            btnChatSearch.UseVisualStyleBackColor = false;
+            // 
+            // btnLoginLog
+            // 
+            btnLoginLog.BackColor = Color.FromArgb(119, 136, 115);
+            btnLoginLog.Cursor = Cursors.Hand;
+            btnLoginLog.FlatAppearance.BorderSize = 0;
+            btnLoginLog.FlatStyle = FlatStyle.Flat;
+            btnLoginLog.Font = new Font("맑은 고딕", 10F, FontStyle.Bold);
+            btnLoginLog.ForeColor = Color.FromArgb(210, 220, 182);
+            btnLoginLog.Location = new Point(13, 483);
+            btnLoginLog.Margin = new Padding(4, 5, 4, 5);
+            btnLoginLog.Name = "btnLoginLog";
+            btnLoginLog.Padding = new Padding(19, 0, 0, 0);
+            btnLoginLog.Size = new Size(257, 70);
+            btnLoginLog.TabIndex = 5;
+            btnLoginLog.Text = "로그인기록";
+            btnLoginLog.TextAlign = ContentAlignment.MiddleLeft;
+            btnLoginLog.UseVisualStyleBackColor = false;
+            // 
+            // btnPermission
+            // 
+            btnPermission.BackColor = Color.FromArgb(119, 136, 115);
+            btnPermission.Cursor = Cursors.Hand;
+            btnPermission.FlatAppearance.BorderSize = 0;
+            btnPermission.FlatStyle = FlatStyle.Flat;
+            btnPermission.Font = new Font("맑은 고딕", 10F, FontStyle.Bold);
+            btnPermission.ForeColor = Color.FromArgb(210, 220, 182);
+            btnPermission.Location = new Point(13, 567);
+            btnPermission.Margin = new Padding(4, 5, 4, 5);
+            btnPermission.Name = "btnPermission";
+            btnPermission.Padding = new Padding(19, 0, 0, 0);
+            btnPermission.Size = new Size(257, 70);
+            btnPermission.TabIndex = 6;
+            btnPermission.Text = "권한관리";
+            btnPermission.TextAlign = ContentAlignment.MiddleLeft;
+            btnPermission.UseVisualStyleBackColor = false;
+            // 
+            // pnlContent
+            // 
+            pnlContent.AutoScroll = true;
+            pnlContent.BackColor = Color.FromArgb(241, 243, 224);
+            pnlContent.Dock = DockStyle.Fill;
+            pnlContent.Location = new Point(283, 0);
+            pnlContent.Margin = new Padding(4, 5, 4, 5);
+            pnlContent.Name = "pnlContent";
+            pnlContent.Padding = new Padding(26, 33, 26, 33);
+            pnlContent.Size = new Size(1173, 759);
+            pnlContent.TabIndex = 1;
+            // 
+            // AdminMainForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1456, 759);
+            Controls.Add(pnlContent);
+            Controls.Add(pnlLeft);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "AdminMainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "관리자 패널";
+            pnlLeft.ResumeLayout(false);
+            ResumeLayout(false);
         }
     }
 }
