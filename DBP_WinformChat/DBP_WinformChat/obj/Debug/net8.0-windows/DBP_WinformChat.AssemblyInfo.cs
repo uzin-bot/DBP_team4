6562a299338ff1deaf06347ed0283bdacb80a9a2
5762981 <5762981@kmu.kr>
2025-12-08 02:57:48 +0900

    zip 파일 오류 수정

--- DBP_WinformChat/DBP_WinformChat/obj/Debug/net8.0-windows/DBP_WinformChat.AssemblyInfo.cs
+++ DBP_WinformChat/DBP_WinformChat/obj/Debug/net8.0-windows/DBP_WinformChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBP_WinformChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace15acb3d3c452cae2301a7cbead25ebf466cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6841edde1269fdbc9f1ad6c31a8903f2ad9cb467")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBP_WinformChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBP_WinformChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
