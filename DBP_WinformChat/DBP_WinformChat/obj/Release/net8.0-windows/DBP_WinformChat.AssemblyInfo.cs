0d74e45363b02492230542a784b5781d8c2cf9c3
lllhnnn <lllhnnn@P79E>
2025-12-09 18:02:54 +0900

    프로그램 아이콘 추가

--- DBP_WinformChat/DBP_WinformChat/obj/Release/net8.0-windows/DBP_WinformChat.AssemblyInfo.cs
+++ DBP_WinformChat/DBP_WinformChat/obj/Release/net8.0-windows/DBP_WinformChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBP_WinformChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c5e5e21d9ddabdb8494764695a612ff0cedfa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8125a0c2dd2351d97814f50fb980485c72fb2541")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBP_WinformChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBP_WinformChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
